f1f7bd7ad9980b9728851ee8933e4fa8a5936c75
seasonli <season.chopsticks@gmail.com>
2018-05-28 19:03:47 +0800

    feat: inc

--- scripts/spiders/douban_inc.js
+++ scripts/spiders/douban_inc.js
@@ -13,6 +13,7 @@ async function getApartments(pageOffset) {
     method: 'GET',
     uri: `http://www.douban.com/group/shanghaizufang/discussion?start=${pageOffset}`,
     headers: {
+      Cookie: 'dbcl2="4488641:+ZS00vhF/iE"',
       'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
     }
   }
